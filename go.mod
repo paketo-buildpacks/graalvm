ae6cd828b60f274c017baa1a48b8555991c38645
Ben Hale <bhale@pivotal.io>
2020-04-24 11:21:12 -0700

    Initial Project Structure
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paketo-buildpacks/graalvm
+
+go 1.14
---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -d ../go-cache ]]; then
+  GOPATH=$(realpath ../go-cache)
+  export GOPATH
+fi
+
+GOOS="linux" go build -ldflags='-s -w' -o bin/build github.com/paketo-buildpacks/graalvm/cmd/build
+GOOS="linux" go build -ldflags='-s -w' -o bin/class-counter github.com/paketo-buildpacks/libjvm/cmd/class-counter
+GOOS="linux" go build -ldflags='-s -w' -o bin/detect github.com/paketo-buildpacks/graalvm/cmd/detect
+GOOS="linux" go build -ldflags='-s -w' -o bin/link-local-dns github.com/paketo-buildpacks/libjvm/cmd/link-local-dns
+GOOS="linux" go build -ldflags='-s -w' -o bin/security-providers-configurer github.com/paketo-buildpacks/libjvm/cmd/security-providers-configurer
